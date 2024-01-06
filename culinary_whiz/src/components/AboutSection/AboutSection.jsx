fe8294fa7af0fe87de7cb2be4810a7fc04ba7699
Dron Kishor <dronkishor2002@gmail.com>
2024-01-06 21:20:25 +0530

    paragraph

--- culinary_whiz/src/components/AboutSection/AboutSection.jsx
+++ culinary_whiz/src/components/AboutSection/AboutSection.jsx
@@ -9,15 +9,13 @@ const AboutSection = () => {
             </div>
             <div className="arightabout">
               <div className="">
-              Lorem ipsum dolor sit amet consectetur. Dui tortor mauris erat ligula. Vel dolor integer nam ultricies suspendisse dignissim. Vitae morbi varius purus pellentesque in duis odio curabitur sodales. Tellus lorem sed ut mauris integer amet. Suspendisse integer et ante arcu magna vitae aliquam sed. At quam pulvinar platea maecenas. Tincidunt mi auctor molestie scelerisque ullamcorper ultricies elit. Aliquam eu potenti posuere magnis eget. Diam aliquam id pellentesque accumsan purus elementum duis. Egestas risus
-              </div>
+              Welcome to Culinary Whiz – Your Culinary Canvas for Inspired Creations. Unleash the Art of Cooking, Explore Expert-Tested Recipes, and Elevate Your Culinary Craftsmanship. Join Us on a Journey of Flavorful Discoveries and Unparalleled Tastes. Let Your Culinary Whiz Unleash!              </div>
             </div>
         </div>
         <div className="bottomabout">
         <div className="arightabout">
         <div className="">
-              Lorem ipsum dolor sit amet consectetur. Dui tortor mauris erat ligula. Vel dolor integer nam ultricies suspendisse dignissim. Vitae morbi varius purus pellentesque in duis odio curabitur sodales. Tellus lorem sed ut mauris integer amet. Suspendisse integer et ante arcu magna vitae aliquam sed. At quam pulvinar platea maecenas. Tincidunt mi auctor molestie scelerisque ullamcorper ultricies elit. Aliquam eu potenti posuere magnis eget. Diam aliquam id pellentesque accumsan purus elementum duis. Egestas risus
-              </div>
+        Culinary Whiz stands as the premier destination for culinary enthusiasts, providing a platform where passion meets expertise. What sets us apart is our commitment to delivering a curated collection of expert-tested recipes, meticulously crafted to elevate your culinary prowess. We take pride in being the trusted choice of renowned chefs who contribute their invaluable insights, making Culinary Whiz a hub of culinary excellence. Our commitment to precision, authenticity, and a seamless user experience ensures that whether you're a seasoned chef or an aspiring home cook, Culinary Whiz is your reliable companion in the world of gastronomy. Join us in this culinary journey, where trust and taste converge, and let your culinary endeavors reach new heights with Culinary Whiz.              </div>
         </div>
         <div className="aleftabout"><img className='b' src="/About/cake.jpg" alt="" /></div>
            
